5019baf5f2854689853766d8f50bf75942e906ca
Alessandro Lentini <alelentini@live.com>
2019-09-02 21:42:18 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Alessandro Lentini on 02/09/2019.
+//  Copyright © 2019 Alessandro Lentini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
